35fe6bcb20d706eeca38b2086b1087eca82d5b14
webiotsAman <aman.singh@webiots.com>
2026-01-03 10:15:54 +0530

    Updated Next version from 15 to 16

--- next.config.js
+++ next.config.js
@@ -1,27 +1,26 @@
-const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require("next/constants");
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require("next/constants");
 
 module.exports = (phase) => {
   const isDev = phase === PHASE_DEVELOPMENT_SERVER;
-  // when `next build` or `npm run build` is used
   const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== "1";
-  // when `next build` or `npm run build` is used
-  const isStaging = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === "1";
+  const isStaging =
+    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === "1";
 
   const env = {
-    API_PROD_URL: (() => {
+    NEXT_PUBLIC_API_PROD_URL: (() => {
       if (isDev) return "/api/";
-      if (isProd) {
-        // Note: The code below needs to be uncommented, and you should use your domin where your API is hosted.
-        // return 'Enter Your URL here'
-        return "/api/";
-      }
+      if (isProd) return "/api/";
       if (isStaging) return "/api/";
-      return "RESTURL_SPEAKERS:not (isDev,isProd && !isStaging,isProd && isStaging)";
+      return "/api/";
     })(),
-    API_BASE_URL: " /api",
+
+    NEXT_PUBLIC_API_BASE_URL: "/api",
+
+    NEXT_PUBLIC_NPROGRESS_SHOW_SPINNER: "false",
   };
 
-  return {
-    env,
-  };
+  return { env };
 };
--- src/app/(MainBody)/layout.js
+++ src/app/(MainBody)/layout.js
@@ -1,97 +1,92 @@
 "use client";
-import getConfig from "next/config";
-import Head from "next/head";
-import Router from "next/router";
-import NProgress from "nprogress";
+
 import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import NProgress from "nprogress";
 import { ToastContainer } from "react-toastify";
 import Customizer from "../../containers/customizer";
 import "../../index.scss";
+
 import {
   HydrationBoundary,
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
 
+/* --------------------------------------------
+   NProgress config (App Router safe)
+--------------------------------------------- */
 
+NProgress.configure({
+  showSpinner:
+    process.env.NEXT_PUBLIC_NPROGRESS_SHOW_SPINNER === "true",
+});
 
-const { publicRuntimeConfig = {} } = getConfig() || {};
-
-NProgress.configure({ showSpinner: publicRuntimeConfig.NProgressShowSpinner });
-
-Router.onRouteChangeStart = () => {
-  NProgress.start();
-};
-
-Router.onRouteChangeComplete = () => {
-  NProgress.done();
-};
-
-Router.onRouteChangeError = () => {
-  NProgress.done();
-};
-
-function MyFunctionComponent({ children }) {
+/* --------------------------------------------
+   Layout content
+--------------------------------------------- */
+function LayoutContent({ children }) {
+  const pathname = usePathname();
   const [goingUp, setGoingUp] = useState(false);
 
+  /* Route change loader */
   useEffect(() => {
-    // Tap to Top Scroll
+    NProgress.start();
+    return () => {
+      NProgress.done();
+    };
+  }, [pathname]);
+
+  /* Tap-to-top */
+  useEffect(() => {
     const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-      if (currentScrollY > 500) setGoingUp(true);
-      else setGoingUp(false);
+      setGoingUp(window.scrollY > 500);
     };
     window.addEventListener("scroll", handleScroll, { passive: true });
-
     return () => window.removeEventListener("scroll", handleScroll);
-  }, [goingUp]);
-
-  const tapToTop = () => {
-    window.scrollTo({
-      behavior: "smooth",
-      top: 0,
-    });
-  };
+  }, []);
 
   return (
     <>
-      <Head>
-        <title>Unice</title>
-      </Head>
-      <>{children}</>
+      {children}
+
       <div
         className="tap-top"
-        style={goingUp ? { display: "block" } : { display: "none" }}
-        onClick={tapToTop}
+        style={{ display: goingUp ? "block" : "none" }}
+        onClick={() =>
+          window.scrollTo({ top: 0, behavior: "smooth" })
+        }
       >
         <div>
-          <i className="fa fa-angle-double-up"></i>
+          <i className="fa fa-angle-double-up" />
         </div>
       </div>
+
       <div className="buy-now d-block">
         <a
           href="https://1.envato.market/GjA3vm"
           target="_blank"
           className="p-2"
         >
-          <i className="fa fa-shopping-cart text-white"></i>
+          <i className="fa fa-shopping-cart text-white" />
         </a>
       </div>
     </>
   );
 }
 
-export default function MyApp({ children }) {
-const [queryClient] = useState(() => new QueryClient());
+/* --------------------------------------------
+   Root layout export
+--------------------------------------------- */
+export default function RootLayout({ children }) {
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
-      <HydrationBoundary state={children.dehydratedState}>
-        <div>
-          <MyFunctionComponent>{children}</MyFunctionComponent>
-          <Customizer />
-          <ToastContainer />
-        </div>
+      <HydrationBoundary>
+        <LayoutContent>{children}</LayoutContent>
+        <Customizer />
+        <ToastContainer />
       </HydrationBoundary>
     </QueryClientProvider>
   );
